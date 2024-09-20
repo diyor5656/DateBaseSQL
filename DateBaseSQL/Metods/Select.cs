330619c5f4064663e227291c42a2607e780b68b4
diyor5656 <absalomovdiyorbek0@gmail.com>
2024-09-20 19:20:51 +0500

    Добавьте файлы проекта.

---
+++ DateBaseSQL/Metods/Select.cs
@@ -0,0 +1,35 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateBaseSQL.Metods
+{
+    public partial class Select
+    {
+
+        public static void SelectProducts(string connectionString, string selectQuery)
+        {
+            using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connection opened");
+
+                using (NpgsqlCommand command = new NpgsqlCommand(selectQuery, connection))
+                {
+                    NpgsqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string productName = reader.GetString(1);
+                        Console.WriteLine($"Product: {productName}");
+                    }
+                }
+
+                connection.Close();
+                Console.WriteLine("Connection closed");
+            }
+        }
+    }
+}
---
+++ DateBaseSQL/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.IO;
+using Newtonsoft.Json;
+using Npgsql;
+using DateBaseSQL.Metods;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+       
+        File.WriteAllText("appsettings.json", "{\n  \"ConnectionString\": {\n    \"PgConnection\": \"\"\n  }\n}");
+
+        
+        Console.Write("Host (masalan: localhost): ");
+        string host = Console.ReadLine();
+
+        Console.Write("Port (masalan: 5432): ");
+        string port = Console.ReadLine();
+
+        Console.Write("Database nomi (masalan: myDB): ");
+        string database = Console.ReadLine();
+
+        Console.Write("User ID (masalan: postgres): ");
+        string userId = Console.ReadLine();
+
+        Console.Write("Password: ");
+        string password = Console.ReadLine();
+
+       
+        string connectionString = $"Host={host};Port={port};Database={database};User Id={userId};Password={password};";
+
+      
+        var appSettings = new
+        {
+            ConnectionString = new
+            {
+                PgConnection = connectionString
+            }
+        };
+
+        
+        string json = JsonConvert.SerializeObject(appSettings, Formatting.Indented);
+        File.WriteAllText("appsettings.json", json);
+
+       
+        var loadedSettings = JsonConvert.DeserializeObject<dynamic>(File.ReadAllText("appsettings.json"));
+        string pgConnection = loadedSettings.ConnectionString.PgConnection;
+
+       
+        var query = "SELECT * FROM Products";
+        Select.SelectProducts(pgConnection, query);
+    }
+
+    
+    
+}
